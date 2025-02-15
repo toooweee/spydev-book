796135a3c1cd493b781b90c2af859496fcf6b401
toooweee <byprob03@gmail.com>
2025-02-15 13:54:54 +0500

    feat-add-click-res

--- apps/api/src/next-gis/next-gis.controller.ts
+++ apps/api/src/next-gis/next-gis.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards, Res } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { NextGisService } from './next-gis.service';
-import { Response } from 'express';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('next-gis')
@@ -8,20 +7,14 @@ export class NextGisController {
     constructor(private readonly nextGisService: NextGisService) {}
 
     @ApiOperation({ summary: 'Получить все записи' })
-    @ApiResponse({
-        status: 200,
-        description: 'Держи.',
-    })
+    @ApiResponse({ status: 200, description: 'Держи.' })
     @Get('all')
     async getAllRecords(): Promise<any[]> {
         return this.nextGisService.getRecords();
     }
 
     @ApiOperation({ summary: 'Добавить героя' })
-    @ApiResponse({
-        status: 201,
-        description: 'Хорош.',
-    })
+    @ApiResponse({ status: 201, description: 'Хорош.' })
     @Post()
     async addRecord(@Body() data: any): Promise<any> {
         return this.nextGisService.addRecord(data);
@@ -35,5 +28,10 @@ export class NextGisController {
     @Delete(':id')
     async deleteRecord(@Param('id') id: number): Promise<any> {
         return this.nextGisService.deleteRecord(id);
+    }
+
+    @Post('click')
+    async handleClick(@Body() clickData: any): Promise<any> {
+        return this.nextGisService.handleClick(clickData);
     }
 }
